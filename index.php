d44ae88e8dcc3e29b5ca3fbfafcccd794f6b3398
dfawess <31991330+dfawess@users.noreply.github.com>
2017-09-15 17:16:49 +0300

    dfawess

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('GIGALUL');
 $app->initLayout('Centered');
 $button = $app->('Button');
-$button->icon = book;
+$button->icon = 'like';
 $button->set('LUL');
