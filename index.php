c61aa2e379e2a5fa2c9261f2e43f998544788e71
dfawess <31991330+dfawess@users.noreply.github.com>
2017-09-29 17:29:50 +0300

    123

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('GIGALUL');
 $app->initLayout('Centered');
-$button = $app->add('Button');
-$button->set('LUL');
-$button->icon = 'plus';
-$button->link ('http://www.vk.com/');
+for ($i=2; $i<=20;) {
+
+  $button = $app->add('Button');
+  $button->set(' '.$i);
+  $i=$i+2;
+  if(($i/2)%2>=1) {
+  $button->set(['primary'=>TRUE]);
+
+  }
+}
