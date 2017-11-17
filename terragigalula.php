c304f92f2d0b6837afd53bf275c8331837947d4d
dfawess <31991330+dfawess@users.noreply.github.com>
2017-11-17 17:01:58 +0200

    Add files via upload

---
+++ terragigalula.php
@@ -0,0 +1,25 @@
+<?php
+
+$db = new \atk4\data\Persistence_SQL('mysql:dbname=friends;host=localhost','root','');
+
+class Friends extends \atk4\data\Model {
+  public $table = 'user';
+  function init() {
+    parent::init();
+    $this->addField('name');
+    $this->addField('surname');
+    $this->addField('phone_number',['default'=>'+371']);
+    $this->addField('nickname');
+    $this->addField('password',['type'=>'password']);
+    $this->addField('birthday',['type'=>'date']);
+    $this->addField('notes', ['type'=>'text']);
+  }
+}
+$form = $app->layout->add('Form');
+$form->setModel(new Friends($db));
+$form->onSubmit(function($form) {
+  $form->model->save();
+  return $form->success('Record updated');
+}
+}
+);
