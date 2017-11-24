b1e196e4a26a77a2992f07a4767ae8b25b716421
dfawess <31991330+dfawess@users.noreply.github.com>
2017-11-24 17:31:16 +0200

    Add files via upload

--- terragigalula.php
+++ terragigalula.php
@@ -1,15 +1,19 @@
 <?php
-
+require 'vendor/autoload.php';
+$app = new \atk4\ui\App('index');
+$app->initLayout('Centered');
 $db = new \atk4\data\Persistence_SQL('mysql:dbname=friends;host=localhost','root','');
 
+
 class Friends extends \atk4\data\Model {
-  public $table = 'user';
+  public $table = 'friends';
   function init() {
     parent::init();
     $this->addField('name');
     $this->addField('surname');
-    $this->addField('phone_number',['default'=>'+371']);
-    $this->addField('nickname');
+    $this->addField('phone_number',['default'=>371]);
+    $this->addField('email');
+    $this->addField('login');
     $this->addField('password',['type'=>'password']);
     $this->addField('birthday',['type'=>'date']);
     $this->addField('notes', ['type'=>'text']);
@@ -20,8 +24,9 @@ $form->setModel(new Friends($db));
 $form->onSubmit(function($form) {
   $form->model->save();
   return $form->success('Record updated');
-}
-}
-);
+});
 
-$label=$app->add(['Label','Хочу новое']);
+$crud = $app->layout->add('Crud');
+$crud->setModel(new Friends($db));
+$crud->addQuickSearch(['surname']);
+$crud->addQuickSearch(['name']);
