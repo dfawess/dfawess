abc23b5e3d22b1f7edb46f0186b7f2388811366c
dfawess <31991330+dfawess@users.noreply.github.com>
2018-01-19 17:29:23 +0200

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,14 +1,17 @@
 <?php
+
+session_start();
+
 require 'vendor/autoload.php';
 $db = new \atk4\data\Persistence_SQL('mysql:dbname=party;host=localhost','root','');
 
 class Friend extends \atk4\data\Model {
-  public $table = 'friends';
-  function init() {
-    parent::init();
-    $this->addField('name',['required'=>TRUE,'caption'=>'Имя']);
-    $this->addField('surname',['required'=>TRUE,'caption'=>'Фамилия']);
-    $this->addField('phone_number',['caption'=>'Телефон','default'=>'371']);
-    $this->addField('age',['required'=>TRUE,'caption'=>'Возраст']);
-  }
+    public $table = 'friends';
+    function init() {
+        parent::init();
+        $this->addField('name',['caption'=>'Имя','required'=>TRUE]);
+        $this->addField('surname',['caption'=>'Фамилия','required'=>TRUE]);
+        $this->addField('phone_number',['caption'=>'Номер телефона','required'=>TRUE,'default'=>'+371','type'=>'integer']);
+        $this->addField('age',['caption'=>'Возраст','required'=>TRUE,'enum'=>['15','16','17','18']]);
+    }
 }
