8374c29ecc7765c28240d8acb91feaa39a47d655
dfawess <31991330+dfawess@users.noreply.github.com>
2018-02-09 17:33:06 +0200

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,17 +1,27 @@
 <?php
 
-session_start();
+require '../vendor/autoload.php';
+$db = new \atk4\data\Persistence_SQL('mysql:dbname=travel_agency;host=localhost','root','');
 
-require 'vendor/autoload.php';
-$db = new \atk4\data\Persistence_SQL('mysql:dbname=party;host=localhost','root','');
+class Place extends \atk4\data\Model {
+  public $table = 'places';
+  function init() {
+      parent::init();
+      $this->addField('name',['caption'=>'Place','required'=>TRUE]);
+      $this->hasMany('User',new User);
 
-class Friend extends \atk4\data\Model {
-    public $table = 'friends';
-    function init() {
-        parent::init();
-        $this->addField('name',['caption'=>'Имя','required'=>TRUE]);
-        $this->addField('surname',['caption'=>'Фамилия','required'=>TRUE]);
-        $this->addField('phone_number',['caption'=>'Номер телефона','required'=>TRUE,'default'=>'+371','type'=>'integer']);
-        $this->addField('age',['caption'=>'Возраст','required'=>TRUE,'enum'=>['15','16','17','18']]);
-    }
+  }
+}
+class User extends \atk4\data\Model {
+  public $table = 'clients';
+  function init() {
+      parent::init();
+      $this->addField('name',['caption'=>'Name','required'=>TRUE]);
+      $this->addField('surname',['caption'=>'Surname','required'=>TRUE]);
+      $this->addField('email',['caption'=>'Email','required'=>TRUE]);
+      $this->addField('phone_number',['caption'=>'Phone Number','required'=>TRUE,'default'=>'+','type'=>'integer']);
+      $this->addField('depart_date',['caption'=>'Depart Date','required'=>TRUE,'type'=>'date']);
+      $this->addField('arrival_date',['caption'=>'Arrival Date','required'=>TRUE,'type'=>'date']);
+      $this->hasOne('places_id',new Place)->addTitle();
+  }
 }
--- places.php
+++ places.php
@@ -6,16 +6,11 @@ $db = new \atk4\data\Persistence_SQL('mysql:dbname=travel_agency;host=localhost'
 $app = new \atk4\ui\App('Travel Agency');
 $app->initLayout('Admin');
 
-$form = $app->layout->add('Form');
-$form->setModel(new User($db));
+$crud = $app->layout->add('Crud');
+$crud->setModel(new Place($db));
 
 $app->layout->leftMenu->addItem(['Main Menu','icon'=>'book'],['index']);
 
 $app->layout->leftMenu->addItem(['Places','icon'=>'cloud'],['places']);
 
 $app->layout->leftMenu->addItem(['Admin','icon'=>'computer'],['user']);
-
-/*$form->onSubmit(function ($form) {
-  $form->model->save();
-  return new \atk4\ui\jsExpression('document.location = "index.php" ');
-});*/
