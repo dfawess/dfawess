57ccc2a8c298b192e35dcd9f91b20c9947b6a2c8
dfawess <31991330+dfawess@users.noreply.github.com>
2018-03-23 17:14:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 require 'connection.php';
 
 $app = new \atk4\ui\App('Travel Agency');
+$app->initLayout('Centered');
 
 require 'visual.php';
 $name=$_ENV['slovo'];
 $lable=$app->add(['Lable',$name,'massive red']);
 
-$form = $app->layout->add('Form');
+//$form = $app->layout->add('Form');
 
-$form->setModel(new Record($db));
+//$form->setModel(new Record($db));
 
 
 
